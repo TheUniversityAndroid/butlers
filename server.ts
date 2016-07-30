28647618d5eecb89e263c1ab7d77f9b4381c2fd7
Matt Dupree <kmdupr33@gmail.com>
2016-07-29 23:23:25 -0400

    Create failing test for POST jobs

--- server.ts
+++ server.ts
@@ -1,15 +1,41 @@
 import * as http from "http";
 import * as fs from "fs";
 // tslint:disable-next-line:no-require-imports no-var-requires
-const toml = require("toml");
+const toml = require("toml-j0.4");
 
 export default (jobsConfigPath: string): http.Server =>
     http.createServer((req, res) => {
-
-        fs.readFile(jobsConfigPath, "utf-8", (err, data) => {
-            if (err) { res.statusCode = 500; res.end(); return; }
-            const jobConfig = toml.parse(data);
-            res.setHeader("Content-type", "application/json");
-            res.end(JSON.stringify(jobConfig));
-        });
+        switch (req.url) {
+            case "/api/jobs":
+                handleJobsRequest(req, res, jobsConfigPath);
+                break;
+            default:
+                unrecognizedRequest(res);
+                break;
+        }
     });
+
+const unrecognizedRequest = (res: http.ServerResponse) => {
+    res.statusCode = 404;
+    res.end();
+};
+
+const handleJobsRequest = (req: http.IncomingMessage, res: http.ServerResponse, jobsConfigPath: string) => {
+    switch (req.method) {
+        case "GET":
+            fs.readFile(jobsConfigPath, "utf-8", (err, data) => {
+                if (err) { res.statusCode = 500; res.end(); return; }
+                const jobConfig = toml.parse(data);
+                res.setHeader("Content-type", "application/json");
+                res.end(JSON.stringify(jobConfig));
+            });
+            break;
+        case "POST":
+            res.statusCode = 201;
+            res.end();
+            break;
+        default:
+            unrecognizedRequest(res);
+        break;
+    }
+};
--- test/apiSpec.ts
+++ test/apiSpec.ts
@@ -2,32 +2,50 @@ import createServer from "../server";
 // tslint:disable-next-line:no-require-imports no-var-requires
 const request = require("supertest");
 
-describe("/api", function() {
+const sampleJob = {
+    jobs: [
+        {
+            id: 1,
+            name: "University Android",
+            status: "new",
+        },
+    ],
+};
+
+describe("/api", function () {
 
     describe("/jobs", function () {
 
-        context("When there are existing configured jobs", function() {
+        context("When there are existing configured jobs", function () {
 
             describe("GET", function () {
 
                 const server = createServer("test-jobs.toml");
 
-                it("returns those configured jobs", function(done) {
+                it("returns those configured jobs", function (done) {
 
                     request(server)
                         .get("/api/jobs")
                         .set("Accept", "application/json")
-                        .expect(200,
-                            { jobs: [
-                                {
-                                    id: 1,
-                                    name: "University Android",
-                                    status: "new",
-                                },
-                            ]},
-                            done);
+                        .expect(200, sampleJob, done);
                 });
             });
+
+            describe("POST", function () {
+
+                const server = createServer("empty-jobs.toml");
+
+                it("appends the posted job to the list of jobs", function (done) {
+                    request(server)
+                        .post("/api/jobs")
+                        .expect(201);
+
+                    request(server)
+                        .get("/api/jobs")
+                        .expect(200, sampleJob, done);
+                });
+
+            })
         });
-   });
+    });
 });
